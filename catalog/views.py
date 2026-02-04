22879717ac4fc117ccc51529205da8f855ea4c64
Степан Прокопьев <dr.aibolitarise@mail.ru>
2026-02-05 01:58:54 +0900

    Созданы приложение catalog, шаблоны, контроллеры и маршрутизация

---
+++ catalog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        # Получаем данные из формы
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Сообщение от {name} ({phone}): {message}')
+        return HttpResponse('Сообщение отправлено!')
+    return render(request, 'catalog/contacts.html')
