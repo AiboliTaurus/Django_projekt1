797c76681ae162fd1d5d51a42dae3e6ae83bdb53
Степан Прокопьев <dr.aibolitarise@mail.ru>
2026-03-17 06:28:05 +0900

    feat(blog): add LoginRequiredMixin to protect create/update/delete views

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@
 from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 
 from config import settings
 from .models import BlogPost
@@ -10,11 +12,11 @@ from .forms import BlogPostForm
 
 
 class BlogPostListView(ListView):
-    """Список всех опубликованных статей блога"""
+    """Список всех опубликованных статей блога - ОБЩЕДОСТУПНО"""
     model = BlogPost
     template_name = 'blog/blog_list.html'
     context_object_name = 'posts'
-    paginate_by = 9  # 9 статей на странице
+    paginate_by = 9
 
     def get_queryset(self):
         """Выводим только опубликованные статьи"""
@@ -22,7 +24,7 @@ class BlogPostListView(ListView):
 
 
 class BlogPostDetailView(DetailView):
-    """Детальная страница статьи блога"""
+    """Детальная страница статьи блога - ОБЩЕДОСТУПНО"""
     model = BlogPost
     template_name = 'blog/blog_detail.html'
     context_object_name = 'post'
@@ -35,7 +37,7 @@ class BlogPostDetailView(DetailView):
         obj.views_count += 1
         obj.save(update_fields=['views_count'])
 
-        # Дополнительное задание: отправка email при 100 просмотрах
+        # Отправка email при 100 просмотрах
         if obj.views_count == 100:
             self.send_congratulatory_email(obj)
 
@@ -66,7 +68,7 @@ class BlogPostDetailView(DetailView):
             send_mail(
                 subject=subject,
                 message=message,
-                from_email=settings.EMAIL_HOST_USER,
+                from_email=settings.DEFAULT_FROM_EMAIL,
                 recipient_list=[settings.EMAIL_HOST_USER],  # Отправляем себе
                 fail_silently=False,
             )
@@ -75,27 +77,52 @@ class BlogPostDetailView(DetailView):
             print(f"❌ Ошибка отправки email: {e}")
 
 
-class BlogPostCreateView(CreateView):
-    """Создание новой статьи блога"""
+class BlogPostCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    """Создание новой статьи блога - ТОЛЬКО ДЛЯ АВТОРИЗОВАННЫХ"""
     model = BlogPost
     form_class = BlogPostForm
     template_name = 'blog/blog_form.html'
     success_url = reverse_lazy('blog:blog_list')
+    login_url = 'users:login'
+    success_message = 'Статья "%(title)s" успешно создана!'
+
+    def form_valid(self, form):
+        """Дополнительная логика при успешной валидации"""
+        response = super().form_valid(form)
+        messages.success(self.request, self.success_message % {'title': self.object.title})
+        return response
 
 
-class BlogPostUpdateView(UpdateView):
-    """Редактирование статьи блога"""
+class BlogPostUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    """Редактирование статьи блога - ТОЛЬКО ДЛЯ АВТОРИЗОВАННЫХ"""
     model = BlogPost
     form_class = BlogPostForm
     template_name = 'blog/blog_form.html'
+    login_url = 'users:login'
+    success_message = 'Статья "%(title)s" успешно обновлена!'
 
     def get_success_url(self):
         """После успешного редактирования перенаправляем на страницу статьи"""
         return reverse_lazy('blog:blog_detail', kwargs={'pk': self.object.pk})
 
+    def form_valid(self, form):
+        """Дополнительная логика при успешной валидации"""
+        response = super().form_valid(form)
+        messages.success(self.request, self.success_message % {'title': self.object.title})
+        return response
 
-class BlogPostDeleteView(DeleteView):
-    """Удаление статьи блога"""
+
+class BlogPostDeleteView(LoginRequiredMixin, DeleteView):
+    """Удаление статьи блога - ТОЛЬКО ДЛЯ АВТОРИЗОВАННЫХ"""
     model = BlogPost
     template_name = 'blog/blog_confirm_delete.html'
     success_url = reverse_lazy('blog:blog_list')
+    login_url = 'users:login'
+
+    def delete(self, request, *args, **kwargs):
+        """Добавляем сообщение при успешном удалении"""
+        self.object = self.get_object()
+        post_title = self.object.title
+        response = super().delete(request, *args, **kwargs)
+        messages.success(request, f'Статья "{post_title}" успешно удалена!')
+        return response
